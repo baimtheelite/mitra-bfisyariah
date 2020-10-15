ef201b00df0273c30957501b59dc8d823358b5c4
Ibrahim Ahmad Jabar <30390302+baimtheelite@users.noreply.github.com>
2020-10-15 14:00:00 +0700

    Update card-feature.blade.php

--- resources/views/components/card-feature.blade.php
+++ resources/views/components/card-feature.blade.php
@@ -3,6 +3,6 @@
         {{-- <img class="card-img-top rounded-circle mx-auto" src="{{ asset($image) }}" alt=""> --}}
         <i class="fa fa-2x fa-dollar-sign"></i>
         <p class="card-title text-capitalize text-bold">{{ $title }}</p>
-        <p class="card-text" style="color: #9BA5A7; font-size: 0.5em">{{ $text }}</p>
+        <p class="card-text" style="color: #9BA5A7;">{{ $text }}</p>
     </div>
 </div>
